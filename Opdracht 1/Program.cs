fb76830733b02cc0ee4651057788a75e60dceef9
Sofie <sofiebrink@yahoo.com>
2024-02-05 11:44:40 +0100

    Opdracht 1

---
+++ Opdracht 1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Opdracht1 {
+    class House {
+        public int floors { get; set; }
+        public int rooms { get; set; }
+        public float width { get; set; }
+        public float height { get; set; }
+        public float depth { get; set; }
+        public int volume => Convert.ToInt32(width * height * depth);
+
+        // Constructor
+        public House(int pFloors
+                , int pRooms
+                , float pWidth
+                , float pHeight
+                , float pDepth)
+        {
+            floors = pFloors;
+            rooms = pRooms;
+            width = pWidth;
+            height = pHeight;
+            depth = pDepth;
+        }
+
+        public string getHouse() {
+            return $"Dit huis heeft {floors} verdiepingen, {rooms} kamers en heeft een volume van {volume} m3";
+        }
+
+        public string getPrice() {
+            return $"De prijs van het huis is: {1500 * volume}";
+        }
+    }
+
+    class Program {
+        static void Main(string[] args){
+        var tmpHouses = new List<House>{
+            new House(2, 4, 5, 5, 5)
+          , new House(3, 6, 10, 5, 7)
+          , new House(2, 3, 7, 2, 5)
+        };
+
+        foreach(var house in tmpHouses) {
+            Console.WriteLine(house.getHouse());
+            Console.WriteLine(house.getPrice());
+        }
+    }
+    }
+    
+}
+
