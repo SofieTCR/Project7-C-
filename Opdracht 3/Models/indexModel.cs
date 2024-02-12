eb5c8121186cd08e13ba348e80c8f6ee13579d31
Sofie <sofiebrink@yahoo.com>
2024-02-12 11:40:01 +0100

    Opdracht 3

--- Opdracht 3/Models/indexModel.cs
+++ Opdracht 3/Models/indexModel.cs
@@ -2,41 +2,69 @@ namespace Opdracht_3.Models;
 
 public class indexModel
 {
-    public List<object> ShapeList = new List<object>();
+    public List<ScreenShape> ShapeList = new List<ScreenShape>();
     public indexModel() {
         ShapeList.Add(new SquareShape(60, 80 , System.Drawing.Color.SandyBrown));
         ShapeList.Add(new SquareShape(55, 120 , System.Drawing.Color.Green));
         ShapeList.Add(new SquareShape(35, 35 , System.Drawing.Color.Chocolate));
-        ShapeList.Add(new CircleShape(75, 75, System.Drawing.Color.Goldenrod));
-        ShapeList.Add(new CircleShape(60, 35, System.Drawing.Color.DarkGreen));
+        ShapeList.Add(new EllipseShape(75, 75, System.Drawing.Color.Goldenrod));
+        ShapeList.Add(new EllipseShape(60, 35, System.Drawing.Color.DarkGreen));
         ShapeList.Add(new TriangleShape(85, 85 , System.Drawing.Color.Lavender));
         ShapeList.Add(new TriangleShape(125, 85 , System.Drawing.Color.MidnightBlue));
     }
 }
-public class ScreenShape {
-    public int width { get; set; }
-    public int height { get; set; }
-    public System.Drawing.Color color { get; set; }
+public abstract class ScreenShape
+{
+    public int width { get; protected set; }
+    public int height { get; protected set; }
+    public System.Drawing.Color color { get; protected set; }
+
+    public string getColor() {
+        return $"rgb({color.R}, {color.G}, {color.B})";
+    }
+    public abstract string draw();
 }
 
-public class SquareShape : ScreenShape {
+public class SquareShape : ScreenShape
+{
     public SquareShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
-        base.width = pWidth;
-        base.height = pHeight;
-        base.color = pColor;
+        width = pWidth;
+        height = pHeight;
+        color = pColor;
+    }
+
+    public override string draw() {
+        return $@"<svg width=""{width}"" height=""{height}"" xmlns=""http://www.w3.org/2000/svg"">
+                      <rect width=""{width * .95}"" height=""{height * .95}"" style=""fill:{getColor()}""/>
+                  </svg>";
     }
 }
-public class CircleShape : ScreenShape {
-    public CircleShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
-        base.width = pWidth;
-        base.height = pHeight;
-        base.color = pColor;
+public class EllipseShape : ScreenShape
+{
+    public EllipseShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
+        width = pWidth;
+        height = pHeight;
+        color = pColor;
+    }
+    public override string draw() {
+        return $@"<svg width=""{width}"" height=""{height}"" xmlns=""http://www.w3.org/2000/svg"">
+                      <ellipse rx=""{width *.5 * .95}"" ry=""{height *.5 * .95}"" cx=""{width * .5}"" cy=""{height * .5}"" style=""fill:{getColor()}""/>
+                  </svg>";
     }
 }
-public class TriangleShape : ScreenShape {
+public class TriangleShape : ScreenShape
+{
     public TriangleShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
-        base.width = pWidth;
-        base.height = pHeight;
-        base.color = pColor;
+        width = pWidth;
+        height = pHeight;
+        color = pColor;
+    }
+    public override string draw() {
+        return $@"<svg width=""{width}"" height=""{height}"" xmlns=""http://www.w3.org/2000/svg"">
+                      <polygon points=""{width/2}, {height - height * .95}
+                                        {width - width * .95}, {height * .95}
+                                        {width * .95}, {height * .95}
+                            "" style=""fill:{getColor()}""/>
+                  </svg>";
     }
 }
