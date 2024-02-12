686a455cf447b054e567ea4f211f09fc514a1d2c
Sofie <sofiebrink@yahoo.com>
2024-02-12 11:54:34 +0100

    Opdracht 3

--- Opdracht 3/Models/indexModel.cs
+++ Opdracht 3/Models/indexModel.cs
@@ -4,15 +4,16 @@ public class indexModel
 {
     public List<ScreenShape> ShapeList = new List<ScreenShape>();
     public indexModel() {
-        ShapeList.Add(new SquareShape(60, 80 , System.Drawing.Color.SandyBrown));
-        ShapeList.Add(new SquareShape(55, 120 , System.Drawing.Color.Green));
-        ShapeList.Add(new SquareShape(35, 35 , System.Drawing.Color.Chocolate));
+        ShapeList.Add(new RectShape(60, 80 , System.Drawing.Color.SandyBrown));
+        ShapeList.Add(new RectShape(55, 120 , System.Drawing.Color.Green));
+        ShapeList.Add(new RectShape(35, 35 , System.Drawing.Color.Chocolate));
         ShapeList.Add(new EllipseShape(75, 75, System.Drawing.Color.Goldenrod));
         ShapeList.Add(new EllipseShape(60, 35, System.Drawing.Color.DarkGreen));
         ShapeList.Add(new TriangleShape(85, 85 , System.Drawing.Color.Lavender));
         ShapeList.Add(new TriangleShape(125, 85 , System.Drawing.Color.MidnightBlue));
     }
 }
+// The underlying shape template
 public abstract class ScreenShape
 {
     public int width { get; protected set; }
@@ -25,9 +26,11 @@ public abstract class ScreenShape
     public abstract string draw();
 }
 
-public class SquareShape : ScreenShape
+// RectShape, I chose to use one class for drawing both rectangles and squares as making a whole other square class seemed
+// excessive as a square is just a rectangle with equal sides.
+public class RectShape : ScreenShape
 {
-    public SquareShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
+    public RectShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
         width = pWidth;
         height = pHeight;
         color = pColor;
@@ -39,6 +42,8 @@ public class SquareShape : ScreenShape
                   </svg>";
     }
 }
+// EllipseShape, I chose to call this class Ellipse Shape instead of circle shape as this also allows
+// you to draw ellipses alongside circles.
 public class EllipseShape : ScreenShape
 {
     public EllipseShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
@@ -48,10 +53,14 @@ public class EllipseShape : ScreenShape
     }
     public override string draw() {
         return $@"<svg width=""{width}"" height=""{height}"" xmlns=""http://www.w3.org/2000/svg"">
-                      <ellipse rx=""{width *.5 * .95}"" ry=""{height *.5 * .95}"" cx=""{width * .5}"" cy=""{height * .5}"" style=""fill:{getColor()}""/>
+                      <ellipse rx=""{width *.5 * .95}"" ry=""{height *.5 * .95}""
+                               cx=""{width * .5}"" cy=""{height * .5}"" 
+                               style=""fill:{getColor()}""/>
                   </svg>";
     }
 }
+// TriangleShape, a triangle is drawn as a polygon, in this case i didn't call this "PolyShape"
+// as that would change the underlying function too much for my liking, this could be done in the future.
 public class TriangleShape : ScreenShape
 {
     public TriangleShape(int pWidth, int pHeight, System.Drawing.Color pColor) {
