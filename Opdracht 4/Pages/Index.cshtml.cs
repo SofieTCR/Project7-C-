c280d9880efd28a4006779782096e8c8f5df9806
Sofie <sofiebrink@yahoo.com>
2024-02-16 11:03:21 +0100

    Opdracht 4

--- Opdracht 4/Pages/Index.cshtml.cs
+++ Opdracht 4/Pages/Index.cshtml.cs
@@ -1,17 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Opdracht_4;
 
 namespace Opdracht_4.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    public ProductList myProducts = new ProductList();
 
     public IndexModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
 
+        Music tmpMusic;
+        Movie tmpMovie;
+        Game tmpGame;
 
+        tmpMusic = new Music("Test1", 7.09f, 8, 2.67f, "Marco's niewste hits singles!");
+        tmpMusic.setArtist("Artiest 1");
+        tmpMusic.AddNumber("number 1");
+        tmpMusic.AddNumber("number 2");
+        myProducts.AddProduct(tmpMusic);
+
+        tmpMusic = new Music("Test2", 15.13f, 8, 9.23f, "Ali's niewste hits singles, inclusief singles 3 en 4!!!");
+        tmpMusic.setArtist("Artiest 2");
+        tmpMusic.AddNumber("number 3");
+        tmpMusic.AddNumber("number 4");
+        myProducts.AddProduct(tmpMusic);
+
+        tmpMovie = new Movie("Starwars 1", 15.13f, 21, 3.23f, "I have the high ground anakin!!!");
+        tmpMovie.setQuality("DVD");
+        myProducts.AddProduct(tmpMovie);
+
+        tmpMovie = new Movie("Starwars 2", 15.13f, 21, 3.23f, "The return of the Jedi, du du du du du du du du du");
+        tmpMovie.setQuality("Blueray");
+        myProducts.AddProduct(tmpMovie);
+
+        tmpGame = new Game("Call of Duty 1", 7.87f, 21, 2.45f, "De nieuwste COD game van sony waar je iedereen dood maakt!");
+        tmpGame.setGenre("FPS");
+        tmpGame.addRequirement("8gb geheugen");
+        tmpGame.addRequirement("970 GTX");
+        myProducts.AddProduct(tmpGame);
+
+        tmpGame = new Game("Call of Duty 2", 13.92f, 21, 6.35f, "De nieuwste COD game van sony waar je iedereen dood maakt! Nu met nog meer dood!");
+        tmpGame.setGenre("FPS");
+        tmpGame.addRequirement("16gb geheugen");
+        tmpGame.addRequirement("2070 RTX");
+        myProducts.AddProduct(tmpGame);
     }
 
     public void OnGet()
--- Opdracht 4/Products.cs
+++ Opdracht 4/Products.cs
@@ -21,16 +21,9 @@ namespace Opdracht_4
         public int tax { get; private set; }
         public string description { get; private set; }
         public float profit { get; private set; }
-        public string category { get; protected set; }
 
         // Methods
         public abstract string printInfo();
-
-        public abstract string getInfo();
-
-        public void setCategory(string pCategory) {
-            category = pCategory;
-        }
 
         // Constructor
         public Product(string pName
@@ -45,6 +38,113 @@ namespace Opdracht_4
             tax = pTax;
             profit = pProfit;
             description = pDescription;
+        }
+    }
+
+    public class Game : Product
+    {
+        public string genre { get; private set; } = "";
+        public List<string> requirements { get; private set; } = new List<string>();
+
+        public Game(string pName
+              , float pPurchasePrice
+              , int pTax
+              , float pProfit
+              , string pDescription)
+              : base(pName, pPurchasePrice, pTax, pProfit, pDescription)
+        {
+        }
+        public void setGenre(string pGenre) {
+            genre = pGenre;
+        }
+
+        public void addRequirement(string pRequirement) {
+            requirements.Add(pRequirement);
+        }
+        public override string printInfo()
+        {
+            return @$"<tr>
+                          <td>{this.GetType().Name}</td>
+                          <td>{name}</td>
+                          <td>{System.Math.Round(purchasePrice, 2)}</td>
+                          <td>
+                              <ul><li>{genre}</li>
+                                  <li>Extra info
+                                      <ul style=""list-style-type:circle;"">
+                                          {string.Join("", requirements.Select(req => $"<li>{req}</li>"))}
+                                      </ul>
+                                  </li>
+                              </ul>
+                          </td>
+                      </tr>";           
+        }
+    }
+
+    public class Movie : Product
+    {
+        public string quality { get; private set; } = "";
+
+        public Movie(string pName
+              , float pPurchasePrice
+              , int pTax
+              , float pProfit
+              , string pDescription)
+              : base(pName, pPurchasePrice, pTax, pProfit, pDescription)
+        {
+        }
+        public void setQuality(string pQuality) {
+            quality = pQuality;
+        }
+        public override string printInfo()
+        {
+            return @$"<tr>
+                          <td>{this.GetType().Name}</td>
+                          <td>{name}</td>
+                          <td>{System.Math.Round(purchasePrice, 2)}</td>
+                          <td>
+                              <ul><li>{quality}</li>
+                              </ul>
+                          </td>
+                      </tr>";           
+        }
+    }
+
+    public class Music : Product
+    {
+        public string artist { get; private set; } = "";
+        public List<string> numbers { get; private set; } = new List<string>();
+
+        public Music(string pName
+              , float pPurchasePrice
+              , int pTax
+              , float pProfit
+              , string pDescription)
+              : base(pName, pPurchasePrice, pTax, pProfit, pDescription)
+        {
+        }
+        public void setArtist(string pArtist) {
+            artist = pArtist;
+        }
+
+        public void AddNumber(string pNumber) {
+            numbers.Add(pNumber);
+        }
+        public override string printInfo()
+        {
+            return @$"<tr>
+                          <td>{this.GetType().Name}</td>
+                          <td>{name}</td>
+                          <td>{System.Math.Round(purchasePrice, 2)}</td>
+                          <td>
+                              <ul><li>{artist}</li>
+                                  <li>Extra info
+                                      <ul style=""list-style-type:circle;"">
+                                          {string.Join("", numbers.Select(num => $"<li>{num}</li>"))}
+                                      </ul>
+                                  </li>
+                               </ul>
+                          </td>
+                      </tr>";           
         }
     }
 }
